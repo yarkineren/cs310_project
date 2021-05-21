452c46068dcd8195c29906157b980824a44e18c0
yarkin <yarkin70@gmail.com>
2021-05-21 14:45:03 +0300

    added my sha certificate and start to database

---
+++ lib/database.dart
@@ -0,0 +1,8 @@
+import 'package:firebase_database/firebase_database.dart';
+import 'utils/colors.dart';
+
+final databaseRef = FirebaseDatabase.instance.reference();
+DatabaseReference savePost(Post post){
+  var id =databaseRef.child('posts/').push();//yeni post yaratıp databaseye atıyor
+  id.set({});//json yapıyoruz
+}
